dbf76b7cf5515540cfb30aa9dd569a84cd39a785
hrong1 <hrong1@ucsc.edu>
2024-10-14 22:37:41 -0700

    small change

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "D2";
 const app = document.querySelector<HTMLDivElement>("#app")!;
+
+// const canvas = document.getElementById("canvas");
+//const ctx = canvas.getContext("2d");
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
