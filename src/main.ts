4905ba1481bd4761be43f40bca13adaa84a1a988
hrong1 <hrong1@ucsc.edu>
2024-10-15 19:39:02 -0700

    step 3 completed

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,47 @@ const header = document.createElement("h1");
 header.innerHTML = "Sticky Pad";
 app.append(header);
 
+
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+        ctx?.beginPath();
+        ctx?.moveTo(cursor.x, cursor.y);
+        ctx?.lineTo(e.offsetX, e.offsetY);
+        ctx?.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+    cursor.active = false;
+});
+
+canvas.addEventListener("mouseout", () => {
+    cursor.active = false;
+});
+
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+clearButton.addEventListener("click", () => {
+    ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 
